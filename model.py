962816d29f3454b85fe9b5f421448856e8f8e06b
hao wang <57213934+jiekesen@users.noreply.github.com>
2024-11-07 10:16:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -13,7 +13,19 @@ import torch.optim as optim
 import os
 import functools
 
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        super(LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
 
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+        
 class SelfAttention(LightningModule):
     def __init__(self, num_attention_heads, input_size, hidden_size, output_dim=1, kernel_size=3,
                  hidden_dropout_prob=0.5, attention_probs_dropout_prob=0.5, learning_rate=0.001):
